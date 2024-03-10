6f4d3156ae14698203638261f0c5c9ce0f692fef
Nico Kuchling <nico.kuchling@outlook.de>
2024-03-10 12:18:52 +0100

    Shared exceptions for all api modules created

--- src/main/java/com/nicokuchling/wegfest/api/exceptions/InvalidInputException.java
+++ src/main/java/com/nicokuchling/wegfest/api/exceptions/InvalidInputException.java
@@ -1,2 +1,12 @@
-package com.nicokuchling.wegfest.api.exceptions;public class InvalidInputException {
+package com.nicokuchling.wegfest.api.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+
+    public InvalidInputException() {}
+
+    public InvalidInputException(String message) { super(message); }
+
+    public InvalidInputException(String message, Throwable cause) { super(message); }
+
+    public InvalidInputException(Throwable cause) { super(cause); }
 }
--- src/main/java/com/nicokuchling/wegfest/api/exceptions/NotFoundException.java
+++ src/main/java/com/nicokuchling/wegfest/api/exceptions/NotFoundException.java
@@ -1,2 +1,12 @@
-package com.nicokuchling.wegfest.api.exceptions;public class NotFoundException {
+package com.nicokuchling.wegfest.api.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException() {}
+
+    public NotFoundException(String message) { super(message); }
+
+    public NotFoundException(String message, Throwable cause) { super(message, cause); }
+
+    public NotFoundException(Throwable cause) { super(cause); }
 }
